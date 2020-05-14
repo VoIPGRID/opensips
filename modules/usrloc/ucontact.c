87f3a067ebcf3180d27b76ee01f00e28f7e4daf6
Jasper Hafkenscheid <jasper.hafkenscheid@wearespindle.com>
2020-05-14 14:25:29 +0200

    Releasenotes and patch for ucontact insert_update

--- modules/usrloc/ucontact.c
+++ modules/usrloc/ucontact.c
@@ -766,7 +766,7 @@ int db_update_ucontact(ucontact_t* _c)
 
 	CON_PS_REFERENCE(ul_dbh) = &my_ps;
 
-	if (ul_dbf.update(ul_dbh, keys1, 0, vals1, keys2, vals2, 1, 15)<0) {
+	if (ul_dbf.insert_update(ul_dbh, keys2, vals2, 15)<0) {
 		LM_ERR("updating database failed\n");
 		goto out_err;
 	}
